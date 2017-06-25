2efc8ded1a02f9a5eeac7978d77aab580f4016bd
Daniel M <danielbmarques@gmail.com>
2017-06-25 03:45:42 -0300

    Add second attempt

--- JTCalTest/ViewController.swift
+++ JTCalTest/ViewController.swift
@@ -13,35 +13,51 @@ class ViewController: UIViewController, JTAppleCalendarViewDelegate, JTAppleCale
     
     @IBOutlet weak var calendarView: JTAppleCalendarView!
 
-    let dateOffset = 3
+    @IBOutlet weak var offsetStepper: UIStepper!
+    @IBOutlet weak var slownessStepper: UIStepper!
+    @IBOutlet weak var offsetLabel: UILabel!
+    @IBOutlet weak var slownessLabel: UILabel!
+
+    var slowness = 1
+    var dateOffset = 2
+    var startDate: Date?
+    var endDate: Date?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calendarView.minimumLineSpacing = 0
+        calendarView.minimumInteritemSpacing = 0
+        offsetStepper.minimumValue = 1
+        offsetStepper.maximumValue = 10
+        slownessStepper.minimumValue = 1
+        slownessStepper.maximumValue = 10
+        setStartAndEndDates(for: Date())
+        updateLabels()
+    }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         select(date: Date())
     }
 
-    var startDate: Date {
-        let currentDate = calendarView.visibleDates().monthDates.first?.date ?? Date()
-        return currentDate.offset(.month, -dateOffset).startOfMonth
+    func updateLabels() {
+        offsetLabel.text = "Offset: \(dateOffset)"
+        slownessLabel.text = "Slowness: \(slowness)"
     }
 
-    var endDate: Date {
-        let currentDate = calendarView.visibleDates().monthDates.first?.date ?? Date()
-        return currentDate.offset(.month, +dateOffset).endOfMonth
+    func setStartAndEndDates(for date: Date = Date.today) {
+        startDate = date.offset(.month, -dateOffset).startOfMonth
+        endDate = date.offset(.month, dateOffset).endOfMonth
     }
 
     public func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
 
-        print("# configureCalendar")
-        print("first visible date: " +
-            (calendarView.visibleDates().monthDates.first?.date.shortDateString ?? "nil"))
-        print("startDate: \(startDate.shortDateString)")
-        print("endDate:   \(endDate.shortDateString)")
-        print("")
-
-        return ConfigurationParameters(startDate: startDate, endDate: endDate,
-                                       numberOfRows: 6, generateInDates: .forAllMonths,
-                                       generateOutDates: .tillEndOfGrid, firstDayOfWeek: .sunday)
+        return ConfigurationParameters(
+            startDate: startDate ?? Date().offset(.month, -dateOffset).startOfMonth,
+            endDate: endDate ?? Date().offset(.month, dateOffset).endOfMonth,
+            numberOfRows: 6, generateInDates: .forAllMonths,
+            generateOutDates: .tillEndOfGrid, firstDayOfWeek: .sunday
+        )
     }
 
     func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date,
@@ -69,7 +85,7 @@ class ViewController: UIViewController, JTAppleCalendarViewDelegate, JTAppleCale
         cell.selectedView.isHidden = !cellState.isSelected
         cell.selectedView.round()
 
-        cell.dotMarkerView.isHidden = date.day % 2 == 0
+        cell.dotMarkerView.isHidden = slowFunction(slowness, returns: date.day % 2 == 0)
 
         if cellState.isSelected {
             cell.dayLabel.textColor = UIColor.white
@@ -92,6 +108,7 @@ class ViewController: UIViewController, JTAppleCalendarViewDelegate, JTAppleCale
                                                                       weight: UIFontWeightRegular)
             } else {
                 cell.dayLabel.textColor = UIColor.black
+                cell.dotMarkerView.circleColor = UIColor.black
                 cell.dayLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 19,
                                                                       weight: UIFontWeightLight)
             }
@@ -105,35 +122,33 @@ class ViewController: UIViewController, JTAppleCalendarViewDelegate, JTAppleCale
         guard let cell = cell as? CellView else { return }
         configureCell(cell, date: date, cellState: cellState)
 
-        if cellState.isSelected {
-            cell.select()
-        }
+        if cellState.isSelected { cell.select() }
     }
 
     func calendar(_ calendar: JTAppleCalendarView,
                   didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
 
-        if let cell = cell as? CellView {
-            configureCell(cell, date: date, cellState: cellState)
-        }
+        guard let cell = cell as? CellView else { return }
+        configureCell(cell, date: date, cellState: cellState)
     }
 
     func calendar(_ calendar: JTAppleCalendarView,
                   didScrollToDateSegmentWith visibleDates: DateSegmentInfo) {
 
         guard let firstVisibleDate = visibleDates.monthDates.first?.date else { return }
-        if firstVisibleDate =~ Date.today.startOfMonth {
-            self.calendarView.selectDates([Date.today])
+        // With previous versions the date had to be selected here, before calling
+        // celandarView.reloadData(). With the latest version this seems to be fixed and I can
+        // call selectDates down there:
+        if firstVisibleDate.month == startDate?.month || firstVisibleDate.month == endDate?.month {
+            setStartAndEndDates(for: firstVisibleDate)
+            calendarView.reloadData()
+            calendarView.scrollToDate(firstVisibleDate, triggerScrollToDateDelegate: false,
+                                      animateScroll: false) {
+                self.calendarView.selectDates([firstVisibleDate]) // <--- Here
+            }
         } else {
-            self.calendarView.selectDates([firstVisibleDate])
+            self.calendarView.selectDates([firstVisibleDate]) // <--- And here
         }
-        print("---")
-        print("")
-        print("# didScroll")
-        print("")
-        calendarView.reloadData()
-        calendarView.scrollToDate(firstVisibleDate, triggerScrollToDateDelegate: false,
-                                  animateScroll: false)
         self.navigationItem.title = firstVisibleDate.shortDateString
     }
 
@@ -143,16 +158,41 @@ class ViewController: UIViewController, JTAppleCalendarViewDelegate, JTAppleCale
             self.calendarView.selectDates([date], triggerSelectionDelegate: true)
             return
         }
-        let firstVisibleDate = calendarView.visibleDates().monthDates.first?.date
-        if startDate != firstVisibleDate?.offset(.month, -dateOffset).startOfMonth || endDate != firstVisibleDate?.offset(.month, +dateOffset).endOfMonth {
+        let oldStartDate = startDate
+        let oldEndDate = endDate
+        setStartAndEndDates(for: date)
+        if startDate != oldStartDate || endDate != oldEndDate {
             calendarView.reloadData()
         }
         calendarView.scrollToDate(date, triggerScrollToDateDelegate: false,
                                   animateScroll: animated, preferredScrollPosition: nil) {
-                                    if self.calendarView.selectedDates.first !~ date {
-                                        self.calendarView.selectDates([date], triggerSelectionDelegate: true)
-                                    }
+            if self.calendarView.selectedDates.first !~ date {
+                self.calendarView.selectDates([date], triggerSelectionDelegate: true)
+            }
         }
+    }
+
+    func slowFunction(_ slowness: Int = 10, returns: Bool) -> Bool {
+        var array: [Int] = []
+        var arrayArray: [[Int]] = []
+        for i in 0...Int(pow(Double(slowness), 6)) {
+            array.append(i)
+        }
+        for _ in 0...Int(pow(Double(slowness), 6)) {
+            let anotherArray = array
+            arrayArray.append(anotherArray)
+        }
+        return returns
+    }
+
+    @IBAction func offsetChanged(_ sender: Any) {
+        dateOffset = Int(offsetStepper.value)
+        updateLabels()
+    }
+
+    @IBAction func slownessChanged(_ sender: Any) {
+        slowness = Int(slownessStepper.value)
+        updateLabels()
     }
 
 }
